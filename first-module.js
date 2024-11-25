beb95dff9573527be0e31ad80c298530fd8a4f06
Leandro Caballero <leandro.a.caballero@hotmail.com>
2024-11-25 16:26:18 -0300

    first commit

---
+++ first-module.js
@@ -0,0 +1,13 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const secret = 'SUPER SECRET'
+const john = 'john'
+const peter ='peter'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
